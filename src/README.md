fc73583619335f5f3d0665d898170ee6031fd71a
Sotaneum <gnyontu39@gmail.com>
2022-06-27 08:22:03 +0900

    [begin] docker 세팅

--- src/README.md
+++ src/README.md
@@ -1,9 +1,30 @@
 # TS Project
 
-## RUN
+## CLI
+
+### dev
+
+docker에서 ts 파일을 빌드해서 예제 파일을 돌립니다.
 
 ```bash
 npm run dev
 ```
 
 Go to http://localhost:3000
+
+### local-dev
+
+local에서 코드 수정시 실시간으로 동작여부를 확인하는 데모를 실행합니다.
+각 터미널에서 각각 실행해야합니디.
+
+```bash
+// 터미널 1
+npm run local-dev-vite
+
+// 터미널 2
+npm run local-dev-server
+```
+
+### build
+
+해당 명령어는 준비중입니다.
---
+++ vite.config.docker.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+
+const path = require("path");
+
+console.log("docker-config");
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    hmr: false,
+    host: true,
+    proxy: {
+      "/socket.io": {
+        target: "ws://localhost:9001",
+        ws: true,
+      },
+    },
+  },
+});
