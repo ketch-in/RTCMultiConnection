abba83b836ebf8be686030170bfe0d70443cc1de
hsdud <youmr100@gmail.com>
2022-07-31 14:47:56 +0900

    IceServersHandler 이름 수정, 변경한 파일 사용

--- src/RTCMultiConnection.ts
+++ src/RTCMultiConnection.ts
@@ -4,7 +4,7 @@ import MultiPeers from "./MultiPeersHandler";
 import CodecsHandler from "./CodecsHandler";
 import setCordovaAPIs from "./ios-hacks";
 import SocketConnection from "./SocketConnection";
-import IceServersHandler from "./IceServersHandler";
+import IceServersHandler from "./done/IceServersHandler";
 import TranslationHandler from "./done/TranslationHandler";
 import getUserMediaHandler from "./done/getUserMediaHandler";
 import FileProgressBarHandler from "./done/FileProgressBarHandler";
